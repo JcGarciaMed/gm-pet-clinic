c6a47ee69d4fcaccb201ae07ef3d0548d324a66c
JORGE <jcgarciamed@gmail.com>
2021-05-05 13:01:16 -0500

    Refactoring to common crud interface. closes #11.

--- gm-pet-clinic-data/src/main/java/com/greymatter/gmpetclinic/services/PetService.java
+++ gm-pet-clinic-data/src/main/java/com/greymatter/gmpetclinic/services/PetService.java
@@ -5,8 +5,6 @@ import com.greymatter.gmpetclinic.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends  CrudService<Pet, Long> {
+
 }
--- gm-pet-clinic-data/src/main/java/com/greymatter/gmpetclinic/services/VetService.java
+++ gm-pet-clinic-data/src/main/java/com/greymatter/gmpetclinic/services/VetService.java
@@ -1,12 +1,7 @@
 package com.greymatter.gmpetclinic.services;
 
-import com.greymatter.gmpetclinic.model.Pet;
 import com.greymatter.gmpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends  CrudService<Vet, Long>{
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
