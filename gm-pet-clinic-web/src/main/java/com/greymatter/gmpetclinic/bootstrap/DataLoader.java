fb69a9b5639afcc76082db56a02bec93b8eb118e
JORGE <jcgarciamed@gmail.com>
2021-05-20 18:43:42 -0500

    Add PetTypes in with Bootstrap Startup. Closes #32.

--- gm-pet-clinic-web/src/main/java/com/greymatter/gmpetclinic/bootstrap/DataLoader.java
+++ gm-pet-clinic-web/src/main/java/com/greymatter/gmpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.greymatter.gmpetclinic.bootstrap;
 
 import com.greymatter.gmpetclinic.model.Owner;
+import com.greymatter.gmpetclinic.model.PetType;
 import com.greymatter.gmpetclinic.model.Vet;
 import com.greymatter.gmpetclinic.services.OwnerService;
+import com.greymatter.gmpetclinic.services.PetTypeService;
 import com.greymatter.gmpetclinic.services.VetService;
 import com.greymatter.gmpetclinic.services.map.OwnerServiceMap;
 import com.greymatter.gmpetclinic.services.map.VetServiceMap;
@@ -14,15 +16,28 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
-
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatType = petTypeService.save(cat);
+
+        System.out.println("Loading Pet Types");
+
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Jorge");
         owner1.setLastName("Garcia");
