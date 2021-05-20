cb089940f3706dce1da02e07bb534c58707f1606
JORGE <jcgarciamed@gmail.com>
2021-05-20 17:42:34 -0500

    added PetTypeServices. Closes #29.

---
+++ gm-pet-clinic-data/src/main/java/com/greymatter/gmpetclinic/services/PetTypeService.java
@@ -0,0 +1,8 @@
+package com.greymatter.gmpetclinic.services;
+
+import com.greymatter.gmpetclinic.model.PetType;
+import com.greymatter.gmpetclinic.services.CrudService;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ gm-pet-clinic-data/src/main/java/com/greymatter/gmpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,37 @@
+package com.greymatter.gmpetclinic.services.map;
+
+import com.greymatter.gmpetclinic.model.PetType;
+import com.greymatter.gmpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
