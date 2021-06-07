29e8820b9b8e52a127057c9874061e676b08f27a
JORGE <jcgarciamed@gmail.com>
2021-06-07 09:30:14 -0500

    Implemented Map Impl for Visit Service. Closes #45.

---
+++ gm-pet-clinic-data/src/main/java/com/greymatter/gmpetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.greymatter.gmpetclinic.services;
+
+import com.greymatter.gmpetclinic.model.Vet;
+import com.greymatter.gmpetclinic.model.Visit;
+
+public interface VisitService extends  CrudService<Visit, Long>{
+}
